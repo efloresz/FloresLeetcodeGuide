fea51cd6d0380ec96bc727dc67a67651fcc6b704
Emily Flores <110843762+efloresz@users.noreply.github.com>
2023-10-11 13:46:38 -0400

    added notes of approaches

--- LeetCode/Arrays & Hashing/Two Sum/two_sum.py
+++ LeetCode/Arrays & Hashing/Two Sum/two_sum.py
@@ -4,3 +4,27 @@ Purpose: Two Sum Problem
 Date Started: 10-11-23
 Date Modified: 10-11-23
 '''
+
+'''
+NOTES: 
+int nums[]
+int target 
+return indicies -> two nums a + b = target 
+(X): can not use same element twice 
+Eg: input nums = [2, 4, 6, 7, 5], target = 8
+    output: [1, 4]
+Explaination: The elements in index 1 = '4' and 4 = '5' add up to the target. 
+
+Potential approache #1: pointers 
+[2, 4, 6, 7, 5]
+^            ^  -> [2, 5] = 7 (X)
+
+[2, 4, 6, 7, 5]
+    ^     ^  -> [4, 7] = 11 (X)
+Limitations: 
+# odd num array: what number would 6 be compared to? (can not use element twice)
+
+Potential approach #2: Hash map ? Hash table ? 
+TODO: explain logic and approach 
+
+'''
